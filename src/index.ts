56fbaef028f41af84c7b6c823e05a96705c161c7
纯逸 <liyuqiong@cai-inc.com>
2024-11-21 15:20:37 +0800

    测试提交

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@
  * @Author: 纯逸 liyuqiong@cai-inc.com
  * @Date: 2024-11-20 19:46:10
  * @LastEditors: 纯逸 liyuqiong@cai-inc.com
- * @LastEditTime: 2024-11-20 19:47:06
+ * @LastEditTime: 2024-11-21 15:20:10
  */
 export function random(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
